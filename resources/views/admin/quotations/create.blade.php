29ed87d44ec110c11e42c2e5d0f7eda7103e7aed
MKCrossMK <mikydelacruz1@gmail.com>
2021-12-29 09:17:30 -0400

    update

--- resources/views/admin/quotations/create.blade.php
+++ resources/views/admin/quotations/create.blade.php
@@ -112,7 +112,7 @@
                                           </div>
                                         </td>
                                         <td >
-                                            <div class="form-group">
+                                            <div style="width: 200px" class="form-group">
                                             <label for="code_referencia">Referencia</label>
                                             <input type="text"  id="code_referencia"  class="form-control" >
                                           </div>
