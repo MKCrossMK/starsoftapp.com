dcc37d5b02e95c347a50e3e5827b2be89a82abe2
MKCrossMK <mikydelacruz1@gmail.com>
2021-12-29 09:22:10 -0400

    update

--- resources/views/admin/clients/show.blade.php
+++ resources/views/admin/clients/show.blade.php
@@ -3,7 +3,7 @@
             
             <div style="margin-top: 5%" class="card" id = "stores">
               
-                <div class="card-header d-flex justify-content-between align-items-center">
+                <div class="card-header d-flex">
                     <h3 style="font: bold">Detalle de Clientes</h3>
                     <div class="row">
                         <div class="col">
