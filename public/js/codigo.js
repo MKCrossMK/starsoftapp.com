98d6187374c0d7814681c93e7dfe90f6ef1ea1a6
MKCrossMK <mikydelacruz1@gmail.com>
2022-01-17 10:37:33 -0400

    update

--- public/js/codigo.js
+++ public/js/codigo.js
@@ -128,8 +128,6 @@ var product_name = $('#product_name');
 	
 
 
-
-
 function agregar() {
 
     product_id = $("#product_id").val();
@@ -184,18 +182,18 @@ function agregar() {
             cancelarTableProductos();
         } else {
 
-          alert("La cantidad a vender supera el stock.")
-            // Swal.fire({
-            //     type: 'error',
-            //     text: 'La cantidad a vender supera el stock.',
-            // })
+          
+             Swal.fire({
+                type: 'error',
+                text: 'La cantidad a vender supera el stock.',
+            })
         }
     } else {
       alert("Rellene todos los campos del detalle de la venta.")
-        // Swal.fire({
-        //     type: 'error',
-        //     text: 'Rellene todos los campos del detalle de la venta.',
-        // })
+        Swal.fire({
+            type: 'error',
+            text: 'Rellene todos los campos del detalle de la venta.',
+        })
     }
 }
 function limpiar() {
